d2be82e9e67920bfae24a7db94638ff832b7bbbe
sos-luoluo <re_an@qq.com>
2020-11-06 17:55:10 +0800

    解析出参数

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,32 +1,34 @@
 const Koa = require("koa");
-import sequelize from "./db/sequelize";
 const koaCors = require("koa-cors");
+const logger = require('koa-logger')
 const koaBody = require("koa-body");
 const bodyParser = require('koa-bodyparser')
-const logger = require('koa-logger')
 import { getUserByToken } from "./auth/auth";
 import router from "./router/router";
-// import './service'
-const app = (module.exports = new Koa());
 
-router.post("/login", (ctx: any) => {
+const app = (module.exports = new Koa());
+router.post("/login", (ctx: any,next:any) => {
     console.log(ctx.request.body);
     ctx.response.body = "success";
 });
 app.use(logger())
-// app.use(bodyParser())
 app.use(koaCors({ credentials: true }));
-
-app.use(async function getUser(ctx: any) {
-    ctx.user = await getUserByToken(ctx.cookies.get("token"));
-});
-app.use(router.routes()).use(router.allowedMethods());
-
-app.use(function pageNotFound(ctx: any) {
-    switch (ctx.accepts("html", "json")) {
-        default:
-            ctx.status = 404;
-            ctx.body = "你要的东西去娜美星了！";
+app.use(async function pageNotFound(ctx: any,next:any) {
+    try {
+       await next()
+        if(!ctx.body){
+            ctx.status=404
+            ctx.body="你要的东西去娜美星了！"
+        }
+    } catch (err) {
+        ctx.status = 500;
+        ctx.body = "系统错误，请稍后再试";
     }
 });
+app.use(koaBody())
+app.use(async function getUser(ctx: any,next:any) {
+    ctx.user = await getUserByToken(ctx.cookies.get("token"));
+    next()
+});
+app.use(router.routes()).use(router.allowedMethods());
 if (!module.parent) app.listen(5000);
